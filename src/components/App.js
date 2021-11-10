a73e8a23b72bd9c35aca1728f9002d167e4c653e
Nick Bechtel <bechtel.nick@gmail.com>
2021-11-10 11:41:21 -0500

    didn't finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
+//API URL
+const apiUrl = "http://localhost:6001/plants";
+
 function App() {
+  //STATE
+
+  const [plants, setPlants] = useState([]);
+
+  //FETCH
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((r) => r.json())
+      .then((data) => {
+        setPlants(data);
+        console.log(data);
+      });
+  }, []);
+
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plantsData={plants} />
     </div>
   );
 }
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard(id, name, image, Price) {
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {Price}</p>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
